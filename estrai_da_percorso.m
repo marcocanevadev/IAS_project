ab199f610ebe39af94cc24e44dd0a3fa41e8977f
marco caneva <marco.caneva.dev@outlook.com>
2023-05-13 15:36:17 +0200

    updated MFFCs func w/ Freq. feats
    modified mfcc function to also concatenate all the freq feats.
    updated extract function to put all feats in a single matrix

--- estrai_da_percorso.m
+++ estrai_da_percorso.m
@@ -1,8 +1,13 @@
-function feats = estrai_da_percorso(path,ext,winlen,stplen)
+function allfeats = estrai_da_percorso(path,ext,winlen,stplen)
 addpath(genpath(pwd))
 list = dir([pwd,path,'/*',ext]);
-feats = [];
+allfeats = [];
+wa = waitbar(length(list));
 for i =1:length(list)
-    mfccs = estraiMFCCs(list(i).name,winlen,stplen);
-    feats = [feats mfccs];
+
+    waitbar(i/length(list));
+    feature = estrai_Freq_MFCCs(list(i).name,winlen,stplen);
+    allfeats = [allfeats feature];
 end
+
+close(wa)
