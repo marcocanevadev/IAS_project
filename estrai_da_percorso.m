f669fd2696a75fa6b221cf95dbf45d83ddec3cda
marco caneva <marco.caneva.dev@outlook.com>
2023-05-13 14:24:10 +0200

    fixed extract_from_path

--- estrai_da_percorso.m
+++ estrai_da_percorso.m
@@ -1,4 +1,8 @@
 function feats = estrai_da_percorso(path,ext,winlen,stplen)
 addpath(genpath(pwd))
 list = dir([pwd,path,'/*',ext]);
-for i 
+feats = [];
+for i =1:length(list)
+    mfccs = estraiMFCCs(list(i).name,winlen,stplen);
+    feats = [feats mfccs];
+end
