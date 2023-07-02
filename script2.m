3598f267e1dc6f0636f293d0c7f87846ac856784
marco caneva <marco.caneva.dev@outlook.com>
2023-07-02 18:53:49 +0200

    cant get around a legend

--- script2.m
+++ script2.m
@@ -5,12 +5,15 @@
 C=[repmat([1 0.2 0],length(BreathTrain_all),1); repmat([0 1 0.5],length(SneezeTrain_all),1); repmat([0.2 0 1], length(SnoreTrain_all),1)];
 figure(1)
 
+
+
+
 scatter3(score(:,1),score(:,2),score(:,3),[],C)
 xlabel('PCA1')
 ylabel('PCA2')
 zlabel('PCA3')
 title('After PCA')
-legend('1','2','3')
+%legend('1','2','3')
 
         %---- Plot Data before PCA ----
 
@@ -27,7 +30,7 @@ xlabel('Feature 1');
 ylabel('Feature 2');
 zlabel('Feature 3');
 title('Before PCA')
-legend('breath','sneeze','snore')
+%legend('breath','sneeze','snore')
 
         %---- Find N° of components that cover 80% of variance ----
 
